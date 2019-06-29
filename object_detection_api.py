04510d6eba8d63533f4dc1fc5aff3355f6fc6ef0
AnhDH <anhdhbn@gmail.com>
2019-06-30 06:43:36 +0700

    fix 500

--- object_detection_api.py
+++ object_detection_api.py
@@ -73,7 +73,7 @@ with detection_graph.as_default():
     detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
     detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
     num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-
+sess = tf.Session()
 # added to put object in JSON
 class Object(object):
     def __init__(self):
@@ -86,17 +86,6 @@ def get_objects(image, threshold=0.5):
     image_np = load_image_into_numpy_array(image)
     # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
     image_np_expanded = np.expand_dims(image_np, axis=0)
-
-
-    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-    # Each box represents a part of the image where a particular object was detected.
-    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-    # Each score represent how level of confidence for each of the objects.
-    # Score is shown on the result image, together with the class label.
-    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
-    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-
     
     # Actual detection.
     # (boxes, scores, classes, num) = sess.run(
