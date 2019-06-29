ece6b6a688e3d29f5089529417a2499f61296cd2
AnhDH <anhdhbn@gmail.com>
2019-06-30 06:40:34 +0700

    fix 500

--- object_detection_api.py
+++ object_detection_api.py
@@ -86,6 +86,18 @@ def get_objects(image, threshold=0.5):
     image_np = load_image_into_numpy_array(image)
     # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
     image_np_expanded = np.expand_dims(image_np, axis=0)
+
+
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    # Each box represents a part of the image where a particular object was detected.
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+    # Each score represent how level of confidence for each of the objects.
+    # Score is shown on the result image, together with the class label.
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+    
     # Actual detection.
     # (boxes, scores, classes, num) = sess.run(
     #     [detection_boxes, detection_scores, detection_classes, num_detections],
@@ -95,7 +107,7 @@ def get_objects(image, threshold=0.5):
     # scores = np.squeeze(scores)
     # boxes = np.squeeze(boxes)
     output_dict = sess.run([detection_boxes, detection_scores, detection_classes, num_detections],
-                             feed_dict={image_tensor: image})
+                             feed_dict={image_tensor: image_np_expanded})
 
       # all outputs are float32 numpy arrays, so convert types as appropriate
     num = int(output_dict['num_detections'][0])
